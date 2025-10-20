0aad662ee9fc94f3445f5f17c21d3c3a7b2faa25
Ian Ellis <ianellis015@gmail.com>
2025-10-20 10:00:58 -0700

    cleaning up script

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// document.getElementById("fetchRoutes").addEventListener("click", fetchRoutes);
-
-// async function fetchRoutes() {
-//   try {
-//     const response = await fetch("http://localhost:3000/stops"); // Fetch from our proxy
-
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! Status: ${response.status}`);
-//     }
-
-//     const data = await response.json();
-//     document.getElementById("output").textContent = JSON.stringify(data, null, 2);
-//   } catch (error) {
-//     console.error("Error fetching routes:", error);
-//     document.getElementById("output").textContent = `Error: ${error.message}`;
-//   }
-// }
-
 
 document.getElementById("fetchStops").addEventListener("click", fetchStops);
 
